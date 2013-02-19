0d7869b4d2dcd4b40c0c08a01acbcf19b7cef964
Janos Gyerik <janos@axiom>
2013-02-19 21:49:29 +0100

    mkisofs needs sudo somehow...

--- scripts/repack.sh
+++ scripts/repack.sh
@@ -10,7 +10,7 @@ tmp=squashfs.gz
 { cd $extract; sudo find | cmd sudo cpio -o -H newc; } | gzip -2 > $tmp
 cmd advdef -z4 $tmp
 cmd sudo mv $tmp $squashfs
-cmd mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
+cmd sudo mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
     -boot-info-table -b boot/isolinux/isolinux.bin \
     -c boot/isolinux/boot.cat -o $livecd1 $newiso
 
