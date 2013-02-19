a59e8c764e2fe3813fd0fea4bfb78928a7e15e71
Janos Gyerik <janos@axiom>
2013-02-19 21:45:03 +0100

    create mnt

--- scripts/unpack-iso.sh
+++ scripts/unpack-iso.sh
@@ -6,6 +6,8 @@
 
 cd $(dirname "$0"); . ./common.sh; cd ..
 
+mkdir -p $mnt
+
 if ! ls $squashfs >/dev/null 2>/dev/null; then
     mount | grep $livecd0 >/dev/null || cmd sudo mount $livecd0 $mnt
     cmd mkdir -p $newiso
