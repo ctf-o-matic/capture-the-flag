3542ca97edaace1949460a4995d129ef3b73c651
Guillaume Rosaire <zerog0@gmail.com>
2013-03-02 19:05:13 +0100

    Moved the packages installation in tc to common.sh, modified install-openssh to use it and added install-kmaps for non US keymaps support

--- scripts/common.sh
+++ scripts/common.sh
@@ -14,6 +14,8 @@ ctf1=./ctf1
 ctf1_orig=$ctf1/orig
 ctf1_append=$ctf1/append
 ctf1_motd=$ctf1/motd
+tcz_url=http://distro.ibiblio.org:/tinycorelinux/4.x/x86/tcz
+tcz_dir=./tcz
 
 msg() {
     echo '[*]' $*
@@ -32,6 +34,26 @@ error() {
 exit_if_nonroot() {
     test $(id -u) = 0 || error this script needs to run as root
 }
+
+install_packages() {
+    exit_if_nonroot
+    
+    packages="$1"
+
+    mkdir -p $tcz_dir
+
+    for package in $packages; do
+	msg installing $package
+	target=$tcz_dir/$package.tcz
+	test -f $target || curl -o $target $tcz_url/$package.tcz
+	tce_marker=$extract/usr/local/tce.installed/$package
+	if ! test -f $tce_marker; then
+            unsquashfs -f -d $extract $target
+            touch $tce_marker
+	fi
+    done
+}
+
 
 for i in "$@"; do
     case "$i" in
---
+++ scripts/install-kmaps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+#
+# File:
+# Purpose:
+#
+
+cd $(dirname "$0"); . ./common.sh; cd ..
+
+install_packages 'kmaps'
+
+# eof
