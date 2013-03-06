056efb7b0c9f2a027f7c7d5deffcddc0caf59c0f
Janos Gyerik <janos@xw8400>
2013-03-06 12:38:04 +0100

    make mkisofs work in debian

--- scripts/pack-iso.sh
+++ scripts/pack-iso.sh
@@ -14,11 +14,14 @@ printf '%60s\n\n' "$(git rev-list HEAD -n1)" >> $build_info
 boot_msg0=boot/isolinux/boot.msg
 cat $ctf1/$boot_msg0 $build_info > $newiso/$boot_msg0
 
+# detect mkisofs executable, name genisoimage in debian
+mkisofs=$(which mkisofs genisoimage blah | head -n 1)
+
 tmp=$work/squashfs.gz
 { cd $extract; find | cpio -o -H newc; } | gzip -2 > $tmp
 cmd advdef -z4 $tmp
 cmd mv $tmp $squashfs
-cmd mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
+cmd $mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
     -boot-info-table -b boot/isolinux/isolinux.bin \
     -c boot/isolinux/boot.cat -o $livecd1 $newiso
 
