1cecd3df9a2354e18dd550066fff2c68897b1895
Janos Gyerik <info@titan2x.com>
2013-03-06 09:05:45 +0100

    added md5 and password list

--- scripts/pack-iso.sh
+++ scripts/pack-iso.sh
@@ -16,4 +16,10 @@ cmd mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
     -boot-info-table -b boot/isolinux/isolinux.bin \
     -c boot/isolinux/boot.cat -o $livecd1 $newiso
 
+msg creating $livecd1.md5
+md5sum $livecd1 > $livecd1.md5
+
+msg creating $livecd1.info
+(cd $extract; grep . home/*/.password) > $livecd1.info
+
 # eof
