a13db9f65b8c7790313a7dab1b76460d6fc5a5ec
Janos Gyerik <info+kronos@janosgyerik.com>
2019-07-14 20:12:57 +0200

    Do not clobber custom authorized_keys file

--- ss1/setup/authorize-for-users.sh
+++ ss1/setup/authorize-for-users.sh
@@ -6,6 +6,6 @@ for user; do
     id "$user" >/dev/null || exit 1
 
     mkdir -vp "/home/$user/.ssh"
-    cp -v /root/.ssh/authorized_keys "/home/$user/.ssh"
+    cat /root/.ssh/authorized_keys >> "/home/$user/.ssh/authorized_keys"
     chown -vR "$user" "/home/$user/.ssh"
 done
