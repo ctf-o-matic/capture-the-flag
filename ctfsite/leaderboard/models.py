767b2054234943a326de58c226afdf44893e4248
Janos Gyerik <info+kronos@janosgyerik.com>
2019-07-19 22:06:15 +0200

    Add model to form teams, with tests

--- ctfsite/leaderboard/models.py
+++ ctfsite/leaderboard/models.py
@@ -1,1 +1,47 @@
-from django.db import models
+from django.db import models, transaction
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+MAX_MEMBERS_PER_TEAM = 4
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=80)
+    created_at = models.DateTimeField(default=now, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def is_accepting_members(self):
+        return TeamMember.objects.filter(team=self).count() < MAX_MEMBERS_PER_TEAM
+
+    def is_empty(self):
+        return TeamMember.objects.filter(team=self).count() == 0
+
+    @transaction.atomic
+    def add_member(self, user):
+        if not self.is_accepting_members():
+            raise ValueError(f"Team '{self.name}' is not accepting members")
+
+        TeamMember.objects.create(team=self, user=user)
+
+    @transaction.atomic
+    def remove_member(self, user):
+        TeamMember.objects.filter(team=self, user=user).delete()
+
+        if self.is_empty():
+            self.delete()
+
+
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=now, blank=True)
+
+    def __str__(self):
+        return f"{self.team} - {self.user}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user'], name="uk_user"),
+        ]
--- ctfsite/leaderboard/tests.py
+++ ctfsite/leaderboard/tests.py
@@ -1,3 +1,62 @@
+import random
+import string
+
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Team, TeamMember, MAX_MEMBERS_PER_TEAM
+
+
+def random_alphabetic(length=10):
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
+
+
+def new_user(username=None):
+    if not username:
+        username = random_alphabetic()
+    return User.objects.create(username=username)
+
+
+def new_team():
+    return Team.objects.create(name=random_alphabetic())
+
+
+class TeamModelTests(TestCase):
+    def test_can_create_teams(self):
+        for _ in range(3):
+            t = new_team()
+            for _ in range(MAX_MEMBERS_PER_TEAM):
+                t.add_member(new_user())
+
+        self.assertEqual(3, Team.objects.count())
+        self.assertEqual(3 * MAX_MEMBERS_PER_TEAM, TeamMember.objects.count())
+
+    def test_cannot_create_more_members_than_max(self):
+        t = new_team()
+        for _ in range(MAX_MEMBERS_PER_TEAM):
+            t.add_member(new_user())
+
+        self.assertRaisesMessage(ValueError, f"Team '{t.name}' is not accepting members", t.add_member, new_user())
+
+    def test_cannot_add_same_member_twice(self):
+        t = new_team()
+        user = new_user()
+        t.add_member(user)
+        self.assertRaises(IntegrityError, t.add_member, user)
+
+    def test_same_user_cannot_join_multiple_teams(self):
+        user = new_user()
+        t1 = new_team()
+        t1.add_member(user)
+        t2 = new_team()
+        self.assertRaises(IntegrityError, t2.add_member, user)
+
+    def test_remove_last_member_deletes_team(self):
+        user = new_user()
+        team = new_team()
+        team.add_member(user)
+        self.assertEqual(1, TeamMember.objects.count())
+        team.remove_member(user)
+        self.assertEqual(0, TeamMember.objects.count())
+
