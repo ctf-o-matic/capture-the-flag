0e3c2f11103951f6b5a3fbe9b14898c43fbcf63a
Janos Gyerik <info+kronos@janosgyerik.com>
2019-09-23 21:44:26 +0200

    Add hints to level generator

--- ss1/gen-leaderboard-levels.sh
+++ ss1/gen-leaderboard-levels.sh
@@ -17,8 +17,10 @@ levels=(${levels[@]##*/})
 
 timestamp=$(date "+%Y-%m-%dT%H:%M:%SZ")
 
-for ((i = 1; i < ${#levels[@]}; i++)); do
+for ((i = 1, j = 1; i < ${#levels[@]}; i++)); do
     level=${levels[i]}
+    solution_key=$(cat "levels/$level/solution-key.txt")
+    crack_script=solutions/levels/$solution_key.sh
     password=$(cat "setup/generated/levels/$level/home/.password")
     password_sha1=$(printf "$password" | sha1sum - | awk '{print $1}')
     cat << EOF
@@ -27,9 +29,25 @@ for ((i = 1; i < ${#levels[@]}; i++)); do
   "pk": $i,
   "fields": {
     "name": "$level",
+    "solution_key": "$solution_key",
     "answer": "$password_sha1",
     "created_at": "$timestamp"
   }
 }
 EOF
+    while read hint; do
+        cat << EOF
+{
+  "model": "leaderboard.hint",
+  "pk": $j,
+  "fields": {
+    "level": $i,
+    "text": "$hint",
+    "visible": false,
+    "created_at": "$timestamp"
+  }
+}
+EOF
+        ((j++))
+    done < <(sed -ne 's/"/\\\\"/g' -e 's/^# hint: //p' "$crack_script")
 done | jq -s .
